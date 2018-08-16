966291e71db30370fc58499a03462f5df57e0db0
sharknasuhorse <naka-ryo6459@nifty.com>
2018-08-16 16:00:14 +0900

    add 00 practice

---
+++ pstate/00-practice/main.tf
@@ -0,0 +1,103 @@
+# 問題番号
+variable "PROBLEM" {
+  default = "00-practice"
+}
+
+# チーム番号
+variable "TEAM_LOGIN_ID" {
+  default = "PUT_TEAM_NUMBER_HERE"
+}
+
+variable "WEBHOOK_URL" {
+  default = "https://hooks.slack.com/services/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"
+}
+
+# vnc admin password
+variable "VNC_SERVER_PASSWORD" {
+  default = "PUT_YOUR_PASSWORD_HERE"
+}
+
+# script
+resource sakuracloud_note "vnc-init" {
+  name        = "${var.PROBLEM}-vnc-init-${var.TEAM_LOGIN_ID}"
+  class       = "shell"
+  content     = "${file("start.sh")}"
+  description = "VNCサーバ(踏み台)初期化スクリプト"
+  tags        = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+# vnc archive
+data sakuracloud_archive "vnc-archive" {
+  name_selectors = ["VNC", "IMG", "V3"]
+}
+
+# switch
+resource sakuracloud_switch "vnc-switch" {
+  name = "${var.PROBLEM}-vnc-switch-${var.TEAM_LOGIN_ID}"
+  tags = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+# disks
+resource sakuracloud_disk "vnc-server-disk" {
+  name              = "${var.PROBLEM}-vnc-server-disk-${var.TEAM_LOGIN_ID}"
+  source_archive_id = "${data.sakuracloud_archive.vnc-archive.id}"
+  note_ids          = ["${sakuracloud_note.vnc-init.id}"]
+  tags              = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+# servers
+resource sakuracloud_server "vnc-server" {
+  name            = "${var.PROBLEM}-vnc-server(踏み台)-${var.TEAM_LOGIN_ID}"
+  core            = 2
+  memory          = 2
+  disks           = ["${sakuracloud_disk.vnc-server-disk.id}"]
+  nic             = "shared"
+  additional_nics = ["${sakuracloud_switch.vnc-switch.id}"]
+  tags            = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+# output
+output "vnc_global_ip" {
+  value = "${sakuracloud_server.vnc-server.ipaddress}"
+}
+
+# simple_monitor
+resource sakuracloud_simple_monitor "vnc-monitor" {
+  target = "${sakuracloud_server.vnc-server.ipaddress}"
+
+  health_check = {
+    protocol   = "ping"
+    delay_loop = 60
+  }
+
+  notify_email_enabled = false
+  notify_slack_enabled = true
+  notify_slack_webhook = "${var.WEBHOOK_URL}"
+  tags                 = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+### 以下自由に書く
+
+## archives
+
+data sakuracloud_archive "00-practice-archive" {
+  name_selectors = ["2018-prep1-00-practice"]
+}
+
+## disks
+
+resource sakuracloud_disk "00-practice-disk" {
+  name              = "${var.PROBLEM}-practice-${var.TEAM_LOGIN_ID}"
+  source_archive_id = "${data.sakuracloud_archive.00-practice-archive.id}"
+  tags              = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
+
+## servers
+resource sakuracloud_server "00-practice-server" {
+  name   = "${var.PROBLEM}-practice-${var.TEAM_LOGIN_ID}"
+  core   = 1
+  memory = 1
+  disks  = ["${sakuracloud_disk.00-practice-disk.id}"]
+  nic    = "${sakuracloud_switch.vnc-switch.id}"
+  tags   = ["problem-${var.PROBLEM}", "${var.TEAM_LOGIN_ID}", "pstate_terraform"]
+}
